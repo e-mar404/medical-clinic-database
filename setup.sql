dc45b428b5fffea180d4acc64ad26b8821af06d1
John Pham <phambinouh@gmail.com>
2024-03-02 17:25:38 -0600

    Added unique constraints

--- setup.sql
+++ setup.sql
@@ -6,6 +6,10 @@ CREATE DATABASE CLINIC_DB;
 /* this is only needed in case the db is not selected from the workbench or from the cli mysql tool */
 USE CLINIC_DB;
 
+DROP DATABASE IF EXISTS mdb;
+CREATE DATABASE mdb;
+USE mdb;
+
 /* Drop tables and reinitialize */
 DROP TABLE IF EXISTS Medication;
 DROP TABLE IF EXISTS Appointment;
@@ -36,21 +40,36 @@ CREATE TABLE ContactInformation (
 
 CREATE TABLE Company (
     company_id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
-    company_name VARCHAR(50) NOT NULL
-    /* Location, Contact Information Needed */
+    company_name VARCHAR(50) NOT NULL,
+	address VARCHAR(50),
+    phone_number VARCHAR(50),
+    created DATE,
+    createdby VARCHAR(50),
+    updated DATE,
+    updatedby VARCHAR(50)
 );
 
 CREATE TABLE Clinic (
     clinic_id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
-    clinic_name VARCHAR(50) NOT NULL
-    /* Location, Contact Information Needed */
+    clinic_name VARCHAR(50) NOT NULL,
+	address VARCHAR(50),
+    phone_number VARCHAR(20),
+    created DATE,
+    createdby VARCHAR(50),
+    updated DATE,
+    updatedby VARCHAR(50)
 );
 
 CREATE TABLE ManagedBy (
     company_id INT NOT NULL,
     clinic_id INT NOT NULL,
+    created DATE,
+    createdby VARCHAR(50),
+    updated DATE,
+    updatedby VARCHAR(50),
     CONSTRAINT FK_ManagedBy_company_id FOREIGN KEY (company_id) REFERENCES Company (company_id),
-    CONSTRAINT FK_ManagedBy_clinic_id FOREIGN KEY (clinic_id) REFERENCES Clinic (clinic_id)
+    CONSTRAINT FK_ManagedBy_clinic_id FOREIGN KEY (clinic_id) REFERENCES Clinic (clinic_id),
+    CONSTRAINT UC_ManagedBy_company_id_clinic_id UNIQUE (company_id, clinic_id)
 );
 
 /* Employee Tables */
@@ -107,7 +126,8 @@ CREATE TABLE Patient_MedicalHistory (
     createdby VARCHAR(50),
     updated DATE,
     updatedby VARCHAR(50),
-    CONSTRAINT FK_Patient_MedicalHistory_patient_id FOREIGN KEY (patient_id) REFERENCES Patient (patient_id)
+    CONSTRAINT FK_Patient_MedicalHistory_patient_id FOREIGN KEY (patient_id) REFERENCES Patient (patient_id),
+    CONSTRAINT UC_Patient_MedicalHistory_patient_id UNIQUE (patient_id)
 );
 
 CREATE TABLE Patient_MedicalProcedure (
@@ -132,7 +152,8 @@ CREATE TABLE Patient_InsuranceInformation (
     createdby VARCHAR(50),
     updated DATE,
     updatedby VARCHAR(50),
-    CONSTRAINT FK_Patient_InsuranceInformation_patient_id FOREIGN KEY (patient_id) REFERENCES Patient (patient_id)
+    CONSTRAINT FK_Patient_InsuranceInformation_patient_id FOREIGN KEY (patient_id) REFERENCES Patient (patient_id),
+    CONSTRAINT UC_Patient_InsuranceInformation_patient_id UNIQUE (patient_id)
 );
 
 CREATE TABLE Patient_FinancialInformation (
@@ -141,6 +162,10 @@ CREATE TABLE Patient_FinancialInformation (
     card_number VARCHAR(50) NOT NULL,
     cvv VARCHAR(50) NOT NULL,
     expiration_date DATE NOT NULL,
+	created DATE,
+    createdby VARCHAR(50),
+    updated DATE,
+    updatedby VARCHAR(50),
     CONSTRAINT FK_Patient_FinancialInformation_patient_id FOREIGN KEY (patient_id) REFERENCES Patient (patient_id)
 );
 
@@ -180,6 +205,10 @@ CREATE TABLE Medication (
     medication_name VARCHAR(50),
     medication_date DATE NOT NULL,
     procedure_id INT,
+	created DATE,
+    createdby VARCHAR(50),
+    updated DATE,
+    updatedby VARCHAR(50),
     CONSTRAINT FK_Medication_patient_id FOREIGN KEY (patient_id) REFERENCES Patient (patient_id),
     CONSTRAINT FK_Medication_doctor_id FOREIGN KEY (doctor_id) REFERENCES MEmployee (memployee_id),
     CONSTRAINT FK_Medication_procedure_id FOREIGN KEY (procedure_id) REFERENCES Patient_MedicalProcedure (procedure_id)
