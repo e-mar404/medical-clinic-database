efbb6afe039c3efd5bb23a8e341d3974b3d3f18e
emar <egmarinn@cougarnet.uh.edu>
2024-04-18 14:48:37 -0500

    update data

--- setupData.sql
+++ setupData.sql
@@ -1,71 +1,70 @@
 INSERT INTO 
   Clinic(clinic_name) 
 VALUES 
-  ('clinic 1'),
-  ('clinic 2');
+  ('Clinic (Houston-Downtown)'),
+  ('Clinic (Houston-Pearland)');
 
 INSERT INTO 
   ContactInformation(email_address, phone_number, address) 
 VALUES
-  ('doctor1@medc.org', '1234567890', '1 street dr'),
-  ('doctor2@medc.org', '1234567890', '1 street dr'),
-  ('nurse1@medc.org', '1234567890', '1 street dr'),
-  ('nurse2@medc.org', '1234567890', '1 street dr'),
-  ('specialist1@medc.org', '1234567890', '1 street dr'),
-  ('specialist2@medc.org', '1234567890', '1 street dr'),
-  ('admin1@medc.org', '1234567890', '1 street dr'),
-  ('admin2@medc.org', '1234567890', '1 street dr'),
-  ('receptionist1@medc.org', '1234567890', '1 street dr'),
-  ('receptionist2@medc.org', '1234567890', '1 street dr'),
-  ('patient1@gmail.com', '1234567890', '1 street dr'),
-  ('patient2@gmail.com', '1234567890', '1 street dr'),
-  ('patient3@gmail.com', '1234567890', '1 street dr'),
-  ('patient4@gmail.com', '1234567890', '1 street dr');    
+  ('lmckinney@medc.org', '1234567890', '1 street dr'),
+  ('szalman@medc.org', '1234567890', '1 street dr'),
+  ('mclavin@medc.org', '1234567890', '1 street dr'),
+  ('dpoiltzer@medc.org', '1234567890', '1 street dr'),
+  ('eelbadawi@medc.org', '1234567890', '1 street dr'),
+  ('swong@medc.org', '1234567890', '1 street dr'),
+  ('uramamurthy@medc.org', '1234567890', '1 street dr'),
+  ('nshepley@medc.org', '1234567890', '1 street dr'),
+  ('drogridues@medc.org', '1234567890', '1 street dr'),
+  ('jbailey@medc.org', '1234567890', '1 street dr'),
+  ('emarin@gmail.com', '1234567890', '1 street dr'),
+  ('btuason@gmail.com', '1234567890', '1 street dr'),
+  ('yrodriguez@gmail.com', '1234567890', '1 street dr'),
+  ('mzaker@gmail.com', '1234567890', '1 street dr');    
 
 INSERT INTO
   Employee(email_address, primary_clinic, employee_type, employee_role, specialist, title, first_name, last_name) 
 VALUES 
-  ('doctor1@medc.org', 1, 'medical', 'doctor', false, 'Family Doctor', 'Doctor', 'One'),
-  ('doctor2@medc.org', 2, 'medical', 'doctor', false, 'Family Doctor', 'Doctor', 'Two'),
-  ('specialist1@medc.org', 1, 'medical', 'doctor', true, 'Cardiologist', 'Specialist', 'One'),
-  ('specialist2@medc.org', 2, 'medical', 'doctor', true, 'Dermatologist', 'Specialist', 'Two'),
-  ('admin1@medc.org', 1, 'staff', 'administrator', false, 'Manager', 'Admin', 'One'),
-  ('admin2@medc.org', 2, 'staff', 'administrator', false, 'Manager', 'Admin', 'Two'),
-  ('receptionist1@medc.org', 1, 'staff', 'receptionist', false, 'Receptionist', 'Front', 'Office1'),
-  ('receptionist2@medc.org', 2, 'staff', 'receptionist', false, 'Receptionist', 'Front', 'Office2'),
-  ('nurse1@medc.org', 1, 'medical', 'nurse', false, 'NP', 'Nurse', 'One'),
-  ('nurse2@medc.org', 2, 'medical', 'nurse', false, 'NP', 'Nurse', 'Two');
-
+  ('lmckinney@medc.org', 1, 'medical', 'doctor', false, 'Family Doctor', 'Lyle', 'McKinney'),
+  ('szalman@medc.org', 2, 'medical', 'doctor', false, 'Family Doctor', 'Sandra', 'Zalman'),
+  ('eelbadawi@medc.org', 1, 'medical', 'doctor', true, 'Cardiologist', 'Emran', 'El-Badawi'),
+  ('swong@medc.org', 2, 'medical', 'doctor', true, 'Dermatologist', 'Sissy', 'Wong'),
+  ('uramamurthy@medc.org', 1, 'staff', 'administrator', false, 'Manager', 'Uma', 'Ramamurthy'),
+  ('nshepley@medc.org', 2, 'staff', 'administrator', false, 'Manager', 'Nathan', 'Shepley'),
+  ('drogridues@medc.org', 1, 'staff', 'receptionist', false, 'Receptionist', 'Debra', 'Rodrigues'),
+  ('jbailey@medc.org', 2, 'staff', 'receptionist', false, 'Receptionist', 'Jeremy', 'Bailey'),
+  ('mclavin@medc.org', 1, 'medical', 'nurse', false, 'NP', 'Matt', 'Clavin'),
+  ('dpoiltzer@medc.org', 2, 'medical', 'nurse', false, 'NP', 'David', 'Poiltzer');
 
 INSERT INTO 
   Patient(email_address, first_name, last_name, date_of_birth, primary_doctor_id, gender) 
 Values 
-  ('patient1@gmail.com', 'patient', 'one', DATE '2003-07-13', 1, 'M'),
-  ('patient2@gmail.com', 'patient', 'two', DATE '2003-07-13', 1, 'F'),
-  ('patient3@gmail.com', 'patient', 'three', DATE '2003-07-13', 2, 'M'),
-  ('patient4@gmail.com', 'patient', 'four', DATE '2003-07-13', 2, 'F');
+  ('emarin@gmail.com', 'Emilio', 'Marin', DATE '2003-07-13', 1, 'M'),
+  ('btuason@gmail.com', 'Ben', 'Tuason', DATE '2003-07-13', 1, 'F'),
+  ('yrodriguez@gmail.com', 'Yesenia', 'Rodriguez', DATE '2003-07-13', 2, 'M'),
+  ('mzaker@gmail.com', 'Mohammed', 'Zaker', DATE '2003-07-13', 2, 'F');
 
 INSERT INTO
   Employee_Login(email_address, password, employee_id)
 VALUES
-  ('doctor1@medc.org', 'password', 1),
-  ('doctor2@medc.org', 'password', 2),
-  ('specialist1@medc.org', 'password', 3),
-  ('specialist2@medc.org', 'password', 4),
-  ('admin1@medc.org', 'password', 5),
-  ('admin2@medc.org', 'password', 6),
-  ('receptionist1@medc.org', 'password', 7),
-  ('receptionist2@medc.org', 'password', 8),
-  ('nurse1@medc.org', 'password', 9),
-  ('nurse2@medc.org', 'password', 10);
+  ('lmckinney@medc.org', 'password', 1),
+  ('szalman@medc.org', 'password', 2),
+  ('eelbadawi@medc.org', 'password', 3),
+  ('swong@medc.org', 'password', 4),
+  ('uramamurthy@medc.org', 'password', 5),
+  ('nshepley@medc.org', 'password', 6),
+  ('drogridues@medc.org', 'password', 7),
+  ('jbailey@medc.org', 'password', 8),
+  ('mclavin@medc.org', 'password', 9),
+  ('dpoiltzer@medc.org', 'password', 10);
 
 INSERT INTO
   Patient_Login(email_address, password, patient_id)
 VALUES
-  ('patient1@gmail.com', 'password', 1),
-  ('patient2@gmail.com', 'password', 2),
-  ('patient3@gmail.com', 'password', 3),
-  ('patient4@gmail.com', 'password', 4);
+  ('emarin@gmail.com', 'password', 1),
+  ('btuason@gmail.com', 'password', 2),
+  ('yrodriguez@gmail.com', 'password', 3),
+  ('mzaker@gmail.com', 'password', 4);
 
 INSERT INTO 
 	Patient_FinancialInformation (patient_id, name_on_card, card_number, cvv, expiration_date) 
@@ -81,17 +80,13 @@ VALUES
 INSERT INTO
   Referral(patient_id, doctor_id, reason_for_referral, expiration_date)
 VALUES
-  (1, 3, 'broken heart idk', Date '2024-05-01'),
-  (2, 3, 'High blood pressure + pregnant', Date '2024-05-01');
+  (1, 3, 'had heart attack 2 weeks ago', Date '2024-05-01'),
+  (2, 4, 'Ezcema', Date '2024-05-01');
 
 INSERT INTO 
 	Appointment(appointment_date, appointment_time, appointment_status, clinic_id, patient_id, doctor_id, confirmation)
 VALUES 
-	(DATE '2024-04-18', '14:00', 1, 1, 1, 1, 1),
-  (DATE '2024-04-19', '15:00',1, 1, 2, 1, 1),
-  (DATE '2024-04-17', '09:00', 1, 2, 3, 2, 1),
-  (DATE '2024-04-17', '15:00',1, 1, 4, 1, 1);
-
-INSERT INTO Charges (patient_id, clinic_id, amount, date_charged, paid) VALUES
-			(1, 1, 15.00, '2024-04-18', 0),
-			(1, 1, 100.00, '2024-04-10', 1);
+	(DATE '2024-04-18', '14:00', 'scheduled', 1, 1, 1, 1),
+  (DATE '2024-04-19', '15:00', 'scheduled', 1, 2, 1, 1),
+  (DATE '2024-04-17', '09:00', 'no show', 2, 3, 2, 1),
+  (DATE '2024-04-17', '15:00', 'complete', 1, 4, 1, 1);
--- setupEvents.sql
+++ setupEvents.sql
@@ -1,6 +1,6 @@
 DELIMITER //
 CREATE EVENT CancelNoShowAppointments 
-ON SCHEDULE EVERY 10 SECOND
+ON SCHEDULE EVERY 5 MINUTE
 DO
 BEGIN
   CALL check_for_no_show_appointments('9:00', '20:00');
